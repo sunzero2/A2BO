d12032c0190e3764d5d50625596dc56a7056a587
chanschans <bbo3353@naver.com>
2020-04-28 21:45:42 +0900

    chan
    메인 컨트롤러 다오 서비스 수정중

--- VeganProject/src/com/a2bo/main/controller/MainController.java
+++ VeganProject/src/com/a2bo/main/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.a2bo.main.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.a2bo.main.model.service.MainService;
 
@@ -22,6 +26,11 @@ public class MainController extends HttpServlet {
 		String uri = request.getRequestURI();
 		String conPath = request.getContextPath();
 		String command = uri.substring(conPath.length());
+		RequestDispatcher rd = null;
+		
+		if(command.contains("searchingVg")) {
+			searchingVg(request,response);
+		}
 		
 		
 		
@@ -31,4 +40,21 @@ public class MainController extends HttpServlet {
 		doGet(request, response);
 	}
 
+	
+	private void searchingVg(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/main.jsp");
+		HttpSession session = request.getSession();
+		String vg = request.getParameter("vLId");
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
