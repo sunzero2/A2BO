8249a8b89dcf6b9aa3bd266b2731e4b535977205
kyeongho664 <rudgh664@naver.com>
2020-05-06 16:02:34 +0900

    kyeongho
    kyeongho

--- VeganProject/web/resources/js/main.js
+++ VeganProject/web/resources/js/main.js
@@ -5,63 +5,9 @@ function all(selector, ln) {
 		return document.querySelector(selector);
 	}
 }
-// 나의 단계가 나온 후에 메뉴 카드 리스트가 나오게 하는 기능
-function create(vals) {
-	var tr = document.createElement('tr');
-	var td = document.createElement('td');
-
-	vals.forEach(function (v) {
-		var td = document.createElement('td');
-		td.textContent = v.value;
-		tr.appendChild(td);
-	})
-
-	var dtd = document.createElement('td');
-	dtd.innerHTML = "<div class='menuImage'>" 
-				   +"<img src='http://placehold.it/245x230'></div>" 
-				   +"<div class='menuInfo'><div class='menuName'>메뉴 이름 : </div><div class='menuPoint'>별점 : </div>" 
-				   +"<div class='menuAd'>주소 : </div><button class='ingBtn' onclick='menuInfoBox()'>버튼 상세보기</button>"
-				   +"<a href='https://www.saramin.co.kr/zf_user/' target='menuIf'></a></div></div>"
-	tr.appendChild(dtd);
-
-	return tr;
-}
 
 
 
-// 메뉴 리스트 카드에서 상세보기 했을 때 아이프레임으로 나오게 하는 기능
-function create2(vals) {
-	var tr = document.createElement('tr');
-	var td = document.createElement('td');
-	
-/*	vals.forEach(function (v) {
-		var td = document.createElement('td');
-		td.textContent = v.value;
-		tr.appendChild(td);
-	})
-*/
-	var dtd = document.createElement('td');
-	dtd.innerHTML = "<div><div><button onclick='xbtn();'>x</button></div><iframe name='menuIf' class='menuIfram' src='https://www.iei.or.kr/main/main.kh'></iframe></div>"
-	tr.appendChild(dtd);
-
-	return tr;
-}
-
-function menuList() {
-	var vals = all('input[type=text]', 1);
-	all('.menuCard').appendChild(create(vals));
-}
-
-function menuInfoBox() {
-	var vals = all('input[type=text]', 1);
-	all('.menuInfoBox').appendChild(create2(vals));
-}
-
-
-function xbtn(){
-	var menuInfoBox = document.querySelector('.menuInfoBox');
-	menuInfoBox.close();
-}
 
 // 재료를 중복 3개를 초과해서 선택했을 경우 경고창이 뜨는 기능
 function count_ck(obj) {
